7c8ad3ee07e6343c77938f64a9a2caf7d3ae6dd9
TheVSDev <vspasic03@gmail.com>
2024-07-22 22:06:01 +0200

    fix: responsive navbar

--- src/web/components/Navbar.jsx
+++ src/web/components/Navbar.jsx
@@ -1,45 +1,68 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import Button from "@/web/components/buttons/Button"
+import Icon from "@/web/components/Icon"
 
 const MenuItem = (props) => {
-  const { linkTo, listItemLabel } = props
+  const { linkTo, listItemLabel, onClick } = props
 
   return (
-    <>
-      <Link href={linkTo} className="no-underline">
-        <li className="list-none p-2 hover:bg-blue-600 hover:text-white">
-          {listItemLabel}
-        </li>
-      </Link>
-    </>
+    <Link
+      href={linkTo}
+      onClick={onClick}
+      className="block px-4 py-2 text-gray-800 hover:bg-blue-600 hover:text-white rounded">
+      {listItemLabel}
+    </Link>
   )
 }
+
 const Navbar = () => {
   const router = useRouter()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   const handleSignOut = () => {
     router.push("/")
   }
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-    <header className="border-b-2 bg-slate-100">
-      <div className="flex p-3">
-        <nav className="ms-auto">
-          <ul className="flex h-full gap-16 items-center list-none text-base">
-            <MenuItem
-              linkTo="/backoffice/create-client-pages"
-              listItemLabel="Créer des pages client"
+    <header className="bg-slate-100">
+      <div className="container mx-auto">
+        <nav className="flex items-center justify-between py-2">
+          <div className="md:hidden">
+            <Icon
+              icon="hamburger"
+              className="w-6 h-6 cursor-pointer text-gray-800"
+              onClick={toggleMenu}
             />
-            <MenuItem
-              linkTo="/backoffice/client-pages"
-              listItemLabel="Pages clients"
-            />
-            <MenuItem
-              linkTo="/backoffice/suggestions"
-              listItemLabel="Suggestions"
-            />
+          </div>
+          <div className="flex-grow flex items-center justify-end">
+            <ul
+              className={`flex-col md:flex-row md:flex gap-4 list-none ${
+                isMenuOpen ? "flex" : "hidden"
+              } md:flex`}>
+              <MenuItem
+                linkTo="/backoffice/create-client-pages"
+                listItemLabel="Créer des pages client"
+                onClick={toggleMenu}
+              />
+              <MenuItem
+                linkTo="/backoffice/client-pages"
+                listItemLabel="Pages clients"
+                onClick={toggleMenu}
+              />
+              <MenuItem
+                linkTo="/backoffice/suggestions"
+                listItemLabel="Suggestions"
+                onClick={toggleMenu}
+              />
+            </ul>
             <Button btnLabel="Déconnexion" onClick={handleSignOut} />
-          </ul>
+          </div>
         </nav>
       </div>
     </header>
