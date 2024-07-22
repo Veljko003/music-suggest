832924bf7a559dd47ee094101ff7074cad5ca401
TheVSDev <vspasic03@gmail.com>
2024-07-22 10:12:27 +0200

    test: initial hoc

--- src/hoc/withAuth.js
+++ src/hoc/withAuth.js
@@ -1,29 +1,21 @@
 import { useEffect, useMemo } from "react"
-import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
-import Loader from "@/web/components/Loader"
 
 const withAuth = (WrappedComponent) => {
   const WithAuth = (props) => {
     const session = useMemo(() => ({ user: { id: 1, name: "Admin" } }), [])
-    const { status } = useSession()
     const router = useRouter()
 
     useEffect(() => {
-      if (status === "loading") return // Do nothing while loading
       if (!session) {
         console.log("No session found, redirecting to sign-in")
         router.replace("/sign-in")
       }
-    }, [session, status, router])
-
-    if (status === "loading") {
-      return <Loader />
-    }
+    }, [session, router])
 
     if (!session) {
       console.log("No session, redirecting")
-      return null // Prevent rendering the wrapped component if not authenticated
+      router.replace("/sign-in")
     }
 
     console.log("Session found", session)
